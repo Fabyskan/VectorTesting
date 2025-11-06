377a4af919b05bee98dfb7d8afdd596d55352196
Fabyskan <20fabyskan09@gmail.com>
2025-11-06 15:21:15 +0100

    GRundstruktur Vecotrklasse erstellt

---
+++ src/Vector.java
@@ -0,0 +1,51 @@
+public class Vector {
+    private double x;
+    private double y;
+
+    public Vector() {}
+
+    public Vector(Vector v) {}
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getMagnitude() {
+        return Math.sqrt(x*x + y*y);
+    }
+
+    public Vector asNormalized() {
+        double magnitude = getMagnitude();
+        if(magnitude == 0) {
+            throw new IllegalArgumentException("Magnitude is zero");
+        }
+        return new Vector(x / magnitude, y / magnitude);
+    }
+
+    public Vector add(Vector v) {
+        return new Vector(this.x + v.x, this.y + v.y);
+    }
+
+    public static void fromPolar(double angle, double magnitude){
+        double x = magnitude * Math.cos(angle);
+        double y = magnitude * Math.sin(angle);
+        new Vector(x, y);
+    }
+}
